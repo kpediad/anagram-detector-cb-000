2d462ea7a443bb66cb724e69cef043085ac11bb7
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-23 17:41:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialise(word)
+    self.word = word
+  end
+
+  def match(words_array)
+    words_array.collect{|word| word if word.split("").sort == self.word.split("").sort}
+  end
+
+end
